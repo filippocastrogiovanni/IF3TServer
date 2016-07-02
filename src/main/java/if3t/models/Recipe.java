4ffd11f463e161fb7f13bf5a3d37b2658912c467
Planet910 <planet910@gmail.com>
2016-07-02 16:29:58 +0200

    zczcz

---
+++ src/main/java/if3t/models/Recipe.java
@@ -0,0 +1,218 @@
+package if3t.models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="recipes")
+public class Recipe {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_recipe", nullable = false)
+	private Long id;
+	
+	@Column(name = "id_group", nullable = false)
+	private Long groupId;
+	
+	@OneToOne(/*cascade = CascadeType.ALL*/)
+	@JoinColumn(name = "id")
+	@Column(name = "username", nullable = false)
+	private User user;
+	
+	@OneToOne(/*cascade = CascadeType.ALL*/)
+	@JoinColumn(name = "id")
+	@Column(name = "id_trigger", nullable = false)
+	private Trigger trigger;
+	
+	@OneToOne(/*cascade = CascadeType.ALL*/)
+	@JoinColumn(name = "id")
+	@Column(name = "id_action", nullable = false)
+	private Action action;
+	
+	
+	private String description;
+	
+	@Column(name = "is_public", nullable = false)
+	private Boolean isPublic;
+	
+	@Column(name = "is_enabled", nullable = false)
+	private Boolean isEnabled;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the groupId
+	 */
+	public Long getGroupId() {
+		return groupId;
+	}
+
+	/**
+	 * @param groupId the groupId to set
+	 */
+	public void setGroupId(Long groupId) {
+		this.groupId = groupId;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the trigger
+	 */
+	public Trigger getTrigger() {
+		return trigger;
+	}
+
+	/**
+	 * @param trigger the trigger to set
+	 */
+	public void setTrigger(Trigger trigger) {
+		this.trigger = trigger;
+	}
+
+	/**
+	 * @return the action
+	 */
+	public Action getAction() {
+		return action;
+	}
+
+	/**
+	 * @param action the action to set
+	 */
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the isPublic
+	 */
+	public Boolean getIsPublic() {
+		return isPublic;
+	}
+
+	/**
+	 * @param isPublic the isPublic to set
+	 */
+	public void setIsPublic(Boolean isPublic) {
+		this.isPublic = isPublic;
+	}
+
+	/**
+	 * @return the isEnabled
+	 */
+	public Boolean getIsEnabled() {
+		return isEnabled;
+	}
+
+	/**
+	 * @param isEnabled the isEnabled to set
+	 */
+	public void setIsEnabled(Boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((action == null) ? 0 : action.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
+		result = prime * result + ((trigger == null) ? 0 : trigger.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Recipe))
+			return false;
+		Recipe other = (Recipe) obj;
+		if (action == null) {
+			if (other.action != null)
+				return false;
+		} else if (!action.equals(other.action))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (groupId == null) {
+			if (other.groupId != null)
+				return false;
+		} else if (!groupId.equals(other.groupId))
+			return false;
+		if (trigger == null) {
+			if (other.trigger != null)
+				return false;
+		} else if (!trigger.equals(other.trigger))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
