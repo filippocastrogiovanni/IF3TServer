68b5023c4f6ddbe48cd0772d9e3b0de925d9fe26
Planet910 <planet910@gmail.com>
2016-07-02 16:28:34 +0200

    sdadad

---
+++ src/main/java/if3t/models/Action.java
@@ -0,0 +1,132 @@
+package if3t.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="actions")
+public class Action {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_action", nullable = false)
+	private Long id;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="channelId")
+	@Column(name = "id_channel", nullable = false)
+	private Channel channel;
+	
+	@NotNull
+	private String description;
+	
+	@OneToOne(mappedBy = "action")
+	private Recipe recipe;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+	/**
+	 * @return the recipe
+	 */
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	/**
+	 * @param recipe the recipe to set
+	 */
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	/**
+	 * @return the channel
+	 */
+	public Channel getChannel() {
+		return channel;
+	}
+
+	/**
+	 * @param channel the channel to set
+	 */
+	public void setChannel(Channel channel) {
+		this.channel = channel;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((channel == null) ? 0 : channel.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Action))
+			return false;
+		Action other = (Action) obj;
+		if (channel == null) {
+			if (other.channel != null)
+				return false;
+		} else if (!channel.equals(other.channel))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		return true;
+	}
+
+	
+	
+}
---
+++ src/main/java/if3t/models/Privilege.java
@@ -0,0 +1,10 @@
+package if3t.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users_roles")
+public class Privilege {
+
+}
