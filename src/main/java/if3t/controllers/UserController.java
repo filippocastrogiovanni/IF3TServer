28e7be1850366c3674ec1a6e29043a02591d9ed4
filippocastrogiovanni <filippo.castrogiovanni@gmail.com>
2016-07-04 18:24:01 +0200

    user controller

--- src/main/java/if3t/controllers/UserController.java
+++ src/main/java/if3t/controllers/UserController.java
@@ -1,17 +1,36 @@
 package if3t.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import if3t.models.User;
 import if3t.services.UserService;
 
 @RestController
 @CrossOrigin
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public void createUser(@RequestBody User u) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			User user = userService.getUserByUsername(auth.getName());
+			if (user != null) {
+				//TODO return error
+			}
+		}
+		//TODO fare controlli su user
+		//TODO salvare la password come hash
+		userService.addUser(u);
+	}
+
 }
