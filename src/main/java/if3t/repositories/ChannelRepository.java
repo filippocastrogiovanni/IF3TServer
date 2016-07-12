81aab25c674172e9383776f7c86dede6363366d4
Planet910 <planet910@gmail.com>
2016-07-12 10:40:07 +0200

    fgdfgdfg

--- src/main/java/if3t/repositories/ChannelRepository.java
+++ src/main/java/if3t/repositories/ChannelRepository.java
@@ -11,4 +11,5 @@ public interface ChannelRepository extends Repository<Channel, Long> {
 	public Iterable<Channel> findAll();
 	public Channel findOne(Long id);
 	public List<Channel> findByAuthorizations_User_Id(Long userId);
+	public Channel findByKeyword(String keyword);
 }
