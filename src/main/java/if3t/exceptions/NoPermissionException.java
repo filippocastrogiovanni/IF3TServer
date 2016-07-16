2b234f8b424bf825aadb4fcac43ca8c456b40c4a
filippocastrogiovanni <filippo.castrogiovanni@gmail.com>
2016-07-16 18:59:37 +0200

    dhfgndhns

--- src/main/java/if3t/exceptions/NoPermissionException.java
+++ src/main/java/if3t/exceptions/NoPermissionException.java
@@ -2,7 +2,11 @@ package if3t.exceptions;
 
 public class NoPermissionException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6611415681138237404L;
 
 	public NoPermissionException(){
 		super("ERROR: You don't have permissions to perform this action!");
