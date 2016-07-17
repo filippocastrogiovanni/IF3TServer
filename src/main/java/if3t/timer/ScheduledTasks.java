ecf686db776e8c92c1f56fc08d34b4b2a791f13a
Alessio <alex92_v@hotmail.it>
2016-07-17 23:09:34 +0200

    mj

--- src/main/java/if3t/timer/ScheduledTasks.java
+++ src/main/java/if3t/timer/ScheduledTasks.java
@@ -4,18 +4,36 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+
+import if3t.services.ChannelService;
 
 @Component
 public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    
 
-//    @Scheduled(fixedRate = 5000)
-//    public void reportCurrentTime() {
-//        System.out.println("1: The time is now " + dateFormat.format(new Date()));
-//    }
+    @Autowired
+	private ChannelService channelService;
+/*
+    @Scheduled(fixedRate = 5*60*1000)
+    public void reportCurrentTime() {
+        //System.out.println("1: The time is now " + dateFormat.format(new Date()));
+    	String accessToken = channelService.readUserChannelAuthorization(9L, "facebook");
+    	FacebookClient fbClient = new DefaultFacebookClient(accessToken);
+    	FacebookType publishMessageResponse= fbClient.publish("me/feed", FacebookType.class,
+    	         Parameter.with("message", "Try to post"));
+    	System.out.println("eseguito ora");
+    }
+    */
 //    
 //    @Scheduled(fixedRate = 3000)
 //    public void reportTime() {
