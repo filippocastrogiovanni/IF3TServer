58620e7248189e3edf67cef8f76e681e3ab82e3c
filippocastrogiovanni <filippo.castrogiovanni@gmail.com>
2016-08-05 11:27:00 +0200

    dbwbt

---
+++ src/main/java/if3t/repositories/ActionIngredientRepository.java
@@ -0,0 +1,17 @@
+package if3t.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+import if3t.models.ActionIngredient;
+
+public interface ActionIngredientRepository extends Repository<ActionIngredient, Long> {
+
+	public Iterable<ActionIngredient> findAll();
+	public ActionIngredient findOne(Long id);
+	public List<ActionIngredient> findByRecipe_Id(Long recipeId);
+	public ActionIngredient save(ActionIngredient ai);
+	//FIXME forse il parametro per cancellare è l'id
+	public void delete(ActionIngredient ai);
+}
