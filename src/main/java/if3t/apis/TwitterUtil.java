98d9cb99ff97e27f4295d3920a26c49ce786f55a
Sebastiano <sebastiano.gazze@gmail.com>
2016-09-16 14:35:40 +0200

    ceecece

--- src/main/java/if3t/apis/TwitterUtil.java
+++ src/main/java/if3t/apis/TwitterUtil.java
@@ -11,14 +11,12 @@ import org.springframework.stereotype.Component;
 
 import if3t.models.Authorization;
 import if3t.models.ChannelStatus;
-import if3t.services.AuthorizationService;
 import if3t.services.ChannelStatusService;
 import twitter4j.HashtagEntity;
 import twitter4j.Paging;
 import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
-import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.conf.ConfigurationBuilder;
@@ -28,24 +26,22 @@ public class TwitterUtil
 {	
 	@Autowired
 	private ChannelStatusService channelStatusService;
-	@Autowired
-	private AuthorizationService authorizationService;
-	private final Logger logger = LoggerFactory.getLogger(TwitterUtil.class.getCanonicalName());
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getCanonicalName());
 	
 	//FIXME settare il debug a false alla fine
-	private Twitter getTwitterInstance(Long userId)
+	private Twitter getTwitterInstance(Long userId, Authorization auth)
 	{
 		ConfigurationBuilder conf = new ConfigurationBuilder();
 		conf.setDebugEnabled(true).setOAuthConsumerKey("rLWBxF1x5DwCgMhtFzGckQytZ").
 		setOAuthConsumerSecret("HYAWanoKCvBHTdw7hSjMj8LPvpbwJ2MPCADgTEuhubbgTXGDW2");
 		
-		Authorization auth = authorizationService.getAuthorization(userId, "twitter");
 		return new TwitterFactory(conf.build()).getInstance(new AccessToken(auth.getAccessToken(), auth.getRefreshToken(), userId));
 	}
 	
-	public boolean postTweet(Long userId, String tweet, List<String> hashtags)
+	public boolean postTweet(Long userId, Authorization auth, String tweet, List<String> hashtags)
 	{
-		Twitter twitter = getTwitterInstance(userId);
+		String screenName = null;
+		Twitter twitter = getTwitterInstance(userId, auth);
 		
 		if (hashtags != null && hashtags.size() > 0)
 		{
@@ -59,35 +55,38 @@ public class TwitterUtil
 			
 			tweet.concat(sb.toString());
 		}
-		    
+		   
 		try 
 		{
 			twitter.updateStatus(tweet);
-			logger.info("Successfully updated the status of the user @" + twitter.getScreenName());
+			screenName = twitter.getScreenName();
+			logger.info("Successfully updated the status of the user @" + screenName);
 			return true;
 		} 
 		catch (Throwable t)
 		{
-			logger.error("Failed to update the status of the user", t);
+			logger.error("Failed to update the status of the user" + ((screenName != null && screenName.length() > 0) ? " @" + screenName : ""), t);
 			return false;
 		}
 	}
 	
 	//TODO assicurarsi che venga rispettato il limit rate
-	public List<Status> getNewUsefulTweets(Long userId, String hashtag)
+	public List<Status> getNewUsefulTweets(Long userId, Authorization auth, String hashtag)
 	{
-		Twitter twitter = getTwitterInstance(userId);
+		String screenName = null;
+		Twitter twitter = getTwitterInstance(userId, auth);
 		long lastProcessedTweetId = Long.MIN_VALUE;
 		List<Status> tweetList = new ArrayList<Status>();
 		ChannelStatus twitterStatus = channelStatusService.readChannelStatus(userId, "twitter");
 		
 		try 
 		{
+			screenName = twitter.getScreenName();
             Paging page = new Paging(1, 200, (twitterStatus != null) ? twitterStatus.getSinceRef() : 1);
             ResponseList<Status> statuses = twitter.getHomeTimeline(page);
             
             if (statuses.size() == 0) {
-            	logger.info("There are no new tweets to process of the user @" + twitter.getScreenName());
+            	logger.info("There are no new tweets to inspect of the user @" + screenName);
             }
             
             for (Status status : statuses) 
@@ -113,7 +112,7 @@ public class TwitterUtil
             	}
             	           	
             	//FIXME togliere alla fine
-                System.out.println("Id " + status.getId() + " - @" + status.getUser().getScreenName() + " - " + status.getText());
+                System.out.println("Id " + status.getId() + " - @" + screenName + " - " + status.getText());
                 
                 if (status.getId() > lastProcessedTweetId) {
             		lastProcessedTweetId = status.getId();
@@ -128,7 +127,7 @@ public class TwitterUtil
         } 
 		catch (Throwable t)
 		{
-			logger.error("Failed to inspect new tweets", t);
+			logger.error("Failed to inspect new tweets of the user" + ((screenName != null && screenName.length() > 0) ? " @" + screenName : ""), t);
 			return Collections.emptyList();
 		}
 	}
--- src/main/java/if3t/timer/TwitterTask.java
+++ src/main/java/if3t/timer/TwitterTask.java
@@ -2,11 +2,22 @@ package if3t.timer;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import if3t.apis.TwitterUtil;
+import if3t.models.Authorization;
+import if3t.models.Channel;
+import if3t.models.Recipe;
+import if3t.models.TriggerIngredient;
+import if3t.models.User;
+import if3t.services.ActionIngredientService;
+import if3t.services.AuthorizationService;
+import if3t.services.RecipeService;
+import if3t.services.TriggerIngredientService;
 import twitter4j.Status;
 
 @Component
@@ -14,10 +25,51 @@ public class TwitterTask
 {
 	@Autowired
 	private TwitterUtil twitterUtil;
+	@Autowired
+    private RecipeService recipeService;
+	@Autowired
+	private TriggerIngredientService triggerIngrService;
+	@Autowired
+	private ActionIngredientService actionIngrService;
+	@Autowired
+	private AuthorizationService authService;
+	private final Logger logger = LoggerFactory.getLogger(this.getClass().getCanonicalName());
 	
+	//TODO diversificare i fixedRate dei vari task per non creare dei picchi di lavoro estremi intervallati dal nulla
 	@Scheduled(fixedRate = 1000*60*5)
     public void twitterScheduler() 
 	{
-    	List<Status> tweets = twitterUtil.getNewUsefulTweets(10L, "#if3t");
+    	//List<Status> tweets = twitterUtil.getNewUsefulTweets(10L, "#if3t");
+		List<Recipe> twitterTriggerRecipes = recipeService.getRecipeByTriggerChannel("twitter");
+		
+		for (Recipe recipe : twitterTriggerRecipes)
+		{
+			User user = recipe.getUser();
+			Channel triggerChannel = recipe.getTrigger().getChannel();
+			Authorization authTrigger = authService.getAuthorization(user.getId(), triggerChannel.getKeyword());
+			
+			if (authTrigger == null)
+			{
+				logger.info("Trigger channel (" + triggerChannel.getKeyword() + ") is not enabled for the user " + user.getUsername());
+				continue;
+			}
+			
+			Channel actionChannel = recipe.getAction().getChannel();
+			Authorization authAction = authService.getAuthorization(user.getId(), actionChannel.getKeyword());
+			
+			if (authAction == null)
+			{
+				logger.info("Action channel (" + actionChannel.getKeyword() + ") is not enabled for the user " + user.getUsername());
+				continue;
+			}
+			
+			switch (recipe.getAction().getChannel().getKeyword())
+			{
+				case "gmail":
+				{
+					//List<TriggerIngredient> trigIngredients = recipe.
+				}
+			}
+		}
 	}
 }
