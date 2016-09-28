8a9c8a2663c780ab559c95c20b17315c7b1ccf65
Sebastiano <sebastiano.gazze@gmail.com>
2016-09-28 15:40:46 +0200

    ffrr

--- src/main/java/if3t/timer/FacebookTask.java
+++ src/main/java/if3t/timer/FacebookTask.java
@@ -400,7 +400,7 @@ public class FacebookTask {
 									}
 								}
 								
-								SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+								SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 	
 								String startDate = startDateString + " " + startTimeString;
 								String endDate = endDateString + " " + endTimeString;
